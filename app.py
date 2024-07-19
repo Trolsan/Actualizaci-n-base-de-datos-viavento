02d8f12e91594cceb7c91c1223a783496da52e84
Trolsan <santiagosanchezluna@gmail.com>
2024-07-19 14:27:40 -0500

    no se que mas hacer

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 app = Flask(__name__)
 CORS(app)
 
 # Configuración de la conexión a MongoDB
-client = MongoClient(os.getenv('mongodb://mongo:WCPqrmtCDXLNtIyeMdauXSnyrseRhHlK@mongodb.railway.internal:27017'))
-db = client['test']
-collection = db['usuarios']
-
-@app.route('/')
-def form():
-    return render_template('form.html')
+# Asegúrate de definir tu variable de entorno MONGO_URI en Railway
+client = MongoClient(os.getenv('MONGO_URI'))
+db = client['nombre_base_datos']
+collection = db['nombre_coleccion']
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    data = request.form.to_dict()
+    data = request.json
+    # Guardar los datos en MongoDB
     collection.insert_one(data)
-    return redirect(url_for('success'))
-
-@app.route('/success')
-def success():
-    return "Datos guardados con éxito"
+    return jsonify({"message": "Datos guardados con éxito"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
